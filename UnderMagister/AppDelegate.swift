4334ba49d5e5b1304054300f37c6247895779412
Under_Koen <koenvanstaveren@hotmail.com>
2018-01-12 18:26:30 +0100

    Initial Commit

---
+++ UnderMagister/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UnderMagister
+//
+//  Created by Koen van Staveren on 12/01/2018.
+//  Copyright © 2018 Koen van Staveren. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UnderMagister/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UnderMagister
+//
+//  Created by Koen van Staveren on 12/01/2018.
+//  Copyright © 2018 Koen van Staveren. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
