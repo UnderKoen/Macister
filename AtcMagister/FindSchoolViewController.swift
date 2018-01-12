71d5e604c851ddd6aa852df009690e9e503d2b1f
Under_Koen <koenvanstaveren@hotmail.com>
2018-01-12 20:44:47 +0100

    refactor

---
+++ AtcMagister/FindSchoolViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MagisterViewController.swift
+//  AtcMagister
+//
+//  Created by Koen van Staveren on 12/01/2018.
+//  Copyright © 2018 Koen van Staveren. All rights reserved.
+//
+
+import Cocoa
+
+class FindSchoolViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
+
+extension FindSchoolViewController {
+    static func freshController() -> FindSchoolViewController {
+        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "FindSchool"), bundle: nil)
+        let identifier = NSStoryboard.SceneIdentifier(rawValue: "MagisterViewController")
+        guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? FindSchoolViewController else {
+            fatalError("Why cant i find FindSchoolViewController? - Check Main.storyboard")
+        }
+        return viewcontroller
+    }
+}
